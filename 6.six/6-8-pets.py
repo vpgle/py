17c5be993b78340c2b01308cbcf4f31d4e5e7229
vpgle <i7-8700k@outlook.com>
2019-05-30 21:31:37 +0800

    update 6-8-pets.py

--- 6.six/6-8-pets.py
+++ 6.six/6-8-pets.py
@@ -16,15 +16,14 @@ pet_thr = {
     'master': 'Reagan',
     }
 
-#pets = [pet_one, pet_two, pet_thr]
-for pet in pet_one.keys():
-    print(pet.title() + " is " + pet_one[pet])
+pets = [pet_one, pet_two, pet_thr]
+# for pet in pet_one.keys():
+    # print(pet.title() + " is " + pet_one[pet])
     #print(pet_one[pet].title())
-pets = [pet_one]
-# for pet in pets:
-    # print(type(pet))
-    # for peta, master in pet.items():
-        # print(peta + " is " + master)
-        # print(pets[peta])
-        # print("\n我是" + master + "科" + 
-        # "主人是" + pets[peta]  + '! ')
+#pets = [pet_one]
+for pet in pets:
+#    print(type(pet))
+    print()
+    for peta, master in pet.items():
+        print(peta + " is " + master)
+#        print(pet[peta])
