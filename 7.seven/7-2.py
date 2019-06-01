284fecc21962e5f1a320a6e3c7374b057bb6f900
vpgle <i7-8700k@outlook.com>
2019-06-01 20:49:09 +0800

    add 7.2 7.3

--- 7.seven/7-2.py
+++ 7.seven/7-2.py
@@ -1,3 +1,8 @@
 # -*- coding: gb18030 -*-
 
+message = input("请问几位？ ")
 
+if int(message) > 8:
+    print("抱歉，现在没有空桌")
+else:
+    print("有空桌，请跟我来")
---
+++ 7.seven/7-3.py
@@ -0,0 +1,9 @@
+# -*- coding: gb18030 -*-
+
+ten = input("请输入一个数字： ")
+
+if int(ten) % 10 == 0:
+    print(ten + "是10的倍数")
+else:
+    print(ten + "不是10的倍数")
+    
