e3def59cc97a57c66055b0859c968d49ad451d04
vpgle <i7-8700k@outlook.com>
2019-06-03 17:41:52 +0800

    create 7.2.5

---
+++ 7.seven/7.2.3.py
@@ -0,0 +1,18 @@
+# -*- coding: gb18030 -*-
+'''
+ÕâÊÇ¸ö×¢ÊÍ
+'''
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+    
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+        
+
