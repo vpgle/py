498a911eae231bba76a1e19c01283a96c01df389
vpgle <i7-8700k@outlook.com>
2019-06-07 16:17:07 +0800

    chapter eight

---
+++ 8.eight/8-1.py
@@ -0,0 +1,7 @@
+# -*- coding: gb18030 -*-
+
+def display_message():
+    """学习函数"""
+    print("我在这章中学习函数的定义，调用。")
+
+display_message()
---
+++ 8.eight/8.1.greeter.py
@@ -0,0 +1,8 @@
+# -*- coding:gb18030 -*-
+
+
+def greet_user():
+    """显示简单的问候语"""
+    print("Hello!")
+
+greet_user()
